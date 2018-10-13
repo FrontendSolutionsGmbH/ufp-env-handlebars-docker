fca3db8808dc4c6daba15891354f7ee7a2073f6a
Christian Kleinhuis <ck@digitalgott.de>
2018-10-14 00:48:22 +0200

    K, LOCAL DEV MODE

--- js/builder.js
+++ js/builder.js
@@ -4,20 +4,22 @@ console.log('BUILDING AND STARTING')
 
 process.env.CFG_MENU_0_LABEL = 'hi people'
 process.env.CFG_MENU_1_LABEL = 'this is local'
+process.env.CFG_MENU_2_LABEL = 'this is local2'
 
 const path1 = path.join(process.cwd(), "build.sh")
 const result1 = execSync(path1, {
-    detached: false,
-    stdio: [0, 1, 2]
+    shell:true,
+    detached: false
 })
+
 const path2 = path.join(process.cwd(), "dist", "execute.sh")
 const result2 = execSync(path2, {
-    stdio: 'inherit',
+    shell:true,
     detached: false,
     cwd: path.join(process.cwd() ,'dist')
 })
 
 console.log(path1)
-console.log(result1)
+console.log(result1.toString())
 console.log(path2)
-console.log(result2)
+console.log(result2.toString())
