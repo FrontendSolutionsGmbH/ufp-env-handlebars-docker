3aefd287f2fe77952879de07e37852baea244304
Christian Kleinhuis <ck@digitalgott.de>
2018-10-13 17:10:13 +0200

    functionality of main scripts

--- test.sh
+++ test.sh
@@ -1,27 +1,54 @@
 #!/usr/bin/env bash
 
 INPUT=$@
+TEARDOWN=1
+CREATE=1
+help() {
+  echo " Executes the component tests"
+  echo " Options:"
+  echo "   -t          No Teardown keeps infrastructure up for inspection after test"
+  echo "   -c          recreate images"
 
+}
+
+while getopts 'thc' OPTION; do
+  case $OPTION in
+    t)
+        TEARDOWN=0
+    ;;
+    c)
+        CREATE="-c"
+    ;;
+
+    h)
+        help
+        exit 0
+    ;;
+  esac
+done
 echo "------------------------------------------------------------------------------"
 echo "Bringing up Infrastructure "
 echo "------------------------------------------------------------------------------"
 
-./stack.sh -d service ${INPUT}
-./stack.sh -u service -b ${INPUT}
+./stack.sh -d service ${CREATE}
+./stack.sh -u service -b ${CREATE}
 
 echo "------------------------------------------------------------------------------"
 echo "Executing component test "
 echo "------------------------------------------------------------------------------"
 
-./stack.sh -d test ${INPUT}
-./stack.sh -u test ${INPUT}
+./stack.sh -d test ${CREATE}
+./stack.sh -u test ${CREATE}
 TEST_RESULT=$?
 
 echo "------------------------------------------------------------------------------"
 echo "Shutting down Infrastructure "
 echo "------------------------------------------------------------------------------"
 
-./stack.sh -d service ${INPUT}
+
+if [ "$TEARDOWN" -eq "1" ]; then
+	./stack.sh -d service ${CREATE}
+fi
 
 echo "EXITING WITH ${TEST_RESULT}"
 exit ${TEST_RESULT}
