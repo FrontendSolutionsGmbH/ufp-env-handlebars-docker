2bff2878aa00d9af83acb51f501b0e94712bf77c
Christian Kleinhuis <ck@digitalgott.de>
2018-10-13 10:54:22 +0200

    revert miss

--- js/build.sh
+++ js/build.sh
@@ -8,11 +8,11 @@ rm -rf dist
 echo "------------------------------------------------------------------------------"
 echo "Executing Webpack for server/server.js"
 echo "------------------------------------------------------------------------------"
-webpack --entry ./server/server.js --target node  --output-filename server.js   --output-path server
+webpack --entry ./server/server.js --target node  --output-filename server.js   --output-path dist/server
 echo "------------------------------------------------------------------------------"
 echo "Executing Webpack for handlebars/handlebars.js"
 echo "------------------------------------------------------------------------------"
-webpack --entry ./handlebars/handlebars.js --target node  --output-filename handlebars.js    --output-path handlebars
+webpack --entry ./handlebars/handlebars.js --target node  --output-filename handlebars.js    --output-path dist/handlebars
 
 echo "------------------------------------------------------------------------------"
 echo "Manually copying static assets"
