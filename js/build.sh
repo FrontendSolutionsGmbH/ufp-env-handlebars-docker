fd9a3841a2872553713ea62a330d92ff8cd2989d
Christian Kleinhuis <ck@digitalgott.de>
2018-10-13 15:24:46 +0200

    eslint

--- js/build.sh
+++ js/build.sh
@@ -13,11 +13,11 @@ rm -rf dist
 echo "------------------------------------------------------------------------------"
 echo "Executing Webpack for server/index.js"
 echo "------------------------------------------------------------------------------"
-webpack --entry ./server/server.js --target node  --output-filename index.js   --output-path dist/server  --mode production
+webpack --entry ./src/server/server.js --target node  --output-filename index.js   --output-path dist/server  --mode production
 echo "------------------------------------------------------------------------------"
 echo "Executing Webpack for handlebars/index.js"
 echo "------------------------------------------------------------------------------"
-webpack --entry ./handlebars/handlebars.js --target node  --output-filename index.js    --output-path dist/handlebars --mode production --resolve-alias handlebars=handlebars/dist/handlebars.js
+webpack --entry ./src/handlebars/handlebars.js --target node  --output-filename index.js    --output-path dist/handlebars --mode production --resolve-alias handlebars=handlebars/dist/handlebars.js
 
 echo "------------------------------------------------------------------------------"
 echo "Manually copying static assets"
---
+++ js/src/handlebars/handlebars.js
@@ -0,0 +1,88 @@
+const Handlebars = require('handlebars')
+const rainuEnvParser = require('rainu-env-parser')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const mkdirp = require('mkdirp')
+const defaults = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'default.json')))
+
+/**
+ * one of the handlebars utility methods
+ * after registering the ;default; helper you can use
+ * {{default value defaultValue}}
+ * in your templates
+ *
+ * */
+Handlebars.registerHelper('default', function (value, defaultValue) {
+  return new Handlebars.SafeString(value || defaultValue)
+})
+
+function line () {
+  console.log('-------------------------------------------------------------------------------')
+}
+// Consts
+
+const ufpConfig = rainuEnvParser.parse('UFP_', {
+
+  templatedir: 'template',
+  targetdir: 'public',
+  prefix: 'CFG_'
+
+})
+
+// Parse environment for object to feed to handlebars
+const parsedEnv = rainuEnvParser.parse(ufpConfig.prefix, defaults)
+line()
+console.log('Config Object is *:')
+console.log(JSON.stringify(ufpConfig, null, ' '))
+console.log('Notes:')
+console.log('(** (use UFP_ prefix environment to override))')
+line()
+console.log('Env Object is * **:')
+console.log(JSON.stringify(parsedEnv, null, ' '))
+console.log('Notes:')
+console.log('(* this is used as input for Handlebars)')
+console.log('(** (use CFG_ prefix environment to override))')
+line()
+
+function handleFile (src, dest) {
+  console.log(`Parsing ${src}`)
+  const source = fs.readFileSync(src, 'utf8')
+  var template = Handlebars.compile(source)
+
+  var result = template(parsedEnv)
+  if (!fs.existsSync(path.dirname(dest))) {
+    console.log('Creating dir:', path.dirname(dest))
+    mkdirp.sync(path.dirname(dest))
+  }
+  fs.writeFileSync(dest, result)
+  console.log(`Written ${dest}`)
+}
+
+const help1 = path.join(process.cwd(), ufpConfig.templatedir)
+const help2 = path.join(process.cwd(), ufpConfig.targetdir)
+console.log(`Template dir ${help1}`)
+console.log(`Template dir ${(help2)}`)
+line()
+
+glob('**/**.*',
+  {
+    cwd: help1
+  }, function (er, files) {
+    // files is an array of filenames.
+    // If the `nonull` option is set, and nothing
+    // was found, then files is ["**/*.js"]
+    // er is an error object or null.
+    if (er === null) {
+      if (files.length === 0) {
+        console.log('No templates found')
+      } else {
+        console.log('Found handlebars templates', files.length)
+        files.map(file => {
+          handleFile(help1 + '/' + file, help2 + '/' + file)
+        })
+      }
+    } else {
+      console.log('Error', er)
+    }
+  })
