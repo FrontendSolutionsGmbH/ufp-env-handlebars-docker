b9a2c7c36df904204a97a24b0051556d1c8c841f
Christian Kleinhuis <ck@digitalgott.de>
2018-10-14 00:54:51 +0200

    strange, but nice anyways :D

--- js/builder.js
+++ js/builder.js
@@ -3,20 +3,24 @@ const {execSync} = require('child_process')
 console.log('BUILDING AND STARTING')
 
 process.env.CFG_MENU_0_LABEL = 'hi people'
+process.env.CFG_MENU_0_CHILDREN_0_LABEL = 'hi people1'
+process.env.CFG_MENU_0_CHILDREN_0_CHILDREN_0_LABEL = 'hi people2'
 process.env.CFG_MENU_1_LABEL = 'this is local'
 process.env.CFG_MENU_2_LABEL = 'this is local2'
 
 const path1 = path.join(process.cwd(), "build.sh")
 const result1 = execSync(path1, {
-    shell:true,
+    shell: true,
+    stdio: [0, 1, 2],
     detached: false
 })
 
 const path2 = path.join(process.cwd(), "dist", "execute.sh")
 const result2 = execSync(path2, {
-    shell:true,
+    shell: true,
+    stdio: [0, 1, 2],
     detached: false,
-    cwd: path.join(process.cwd() ,'dist')
+    cwd: path.join(process.cwd(), 'dist')
 })
 
 console.log(path1)
