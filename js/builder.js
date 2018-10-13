43764762fdfe4026e6f4f913d3897bee30db370f
Christian Kleinhuis <ck@digitalgott.de>
2018-10-14 01:40:05 +0200

    roughly working menu, tomorrow ufp css

--- js/builder.js
+++ js/builder.js
@@ -2,28 +2,25 @@ const path = require('path')
 const {execSync} = require('child_process')
 console.log('BUILDING AND STARTING')
 
-process.env.CFG_MENU_0_LABEL = 'hi people'
-process.env.CFG_MENU_0_CHILDREN_0_LABEL = 'hi people1'
-process.env.CFG_MENU_0_CHILDREN_0_CHILDREN_0_LABEL = 'hi people2'
-process.env.CFG_MENU_1_LABEL = 'this is local'
-process.env.CFG_MENU_2_LABEL = 'this is local2'
+process.env.CFG_MENU_0_LABEL = 'people'
+process.env.CFG_MENU_0_CHILDREN_0_LABEL = 'people1'
+process.env.CFG_MENU_0_CHILDREN_0_CHILDREN_0_LABEL = 'people2'
+process.env.CFG_MENU_1_LABEL = 'local'
+process.env.CFG_MENU_2_LABEL = 'local2'
 
 const path1 = path.join(process.cwd(), "build.sh")
 const result1 = execSync(path1, {
     shell: true,
-    stdio: [0, 1, 2],
     detached: false
-})
-
-const path2 = path.join(process.cwd(), "dist", "execute.sh")
-const result2 = execSync(path2, {
-    shell: true,
-    stdio: [0, 1, 2],
-    detached: false,
-    cwd: path.join(process.cwd(), 'dist')
 })
 
 console.log(path1)
 console.log(result1.toString())
+const path2 = path.join(process.cwd(), "dist", "execute.sh")
+const result2 = execSync(path2, {
+    shell: true,
+    cwd: path.join(process.cwd(), 'dist')
+})
+
 console.log(path2)
 console.log(result2.toString())
